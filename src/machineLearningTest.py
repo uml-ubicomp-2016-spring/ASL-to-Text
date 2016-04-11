6d24f29ed5b97d98d14d64a5da39d3b4807d5605
Kevin Dibble <kdibble@cs.uml.edu>
2016-04-11 17:06:15 -0400

    CHANGELOG: KDibble 	Added a simple example that lists data and then predicts off of that data

--- src/machineLearningTest.py
+++ src/machineLearningTest.py
@@ -1,3 +1,16 @@
 from sklearn import datasets
 iris = datasets .load_iris()
 digits = datasets.load_digits()
+
+from sklearn import svm
+import numpy as np
+myData = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+myTargets = np.array([-1, -2], np.int32)
+
+myClf = svm.SVC()
+myClf.fit(myData, myTargets)
+
+print(myClf.predict([[1, 2, 3]]))
+print(myClf.predict([[1, 2, 4]]))
+print(myClf.predict([[4, 5, 6]]))
+print(myClf.predict([[4, 5, 7]]))
